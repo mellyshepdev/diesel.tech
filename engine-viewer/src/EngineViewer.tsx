7a354535c75ddf31bf398235d04d9a9c51f1ca54
Shepherd Devoloper <blackshepherddeveloper@gmail.com>
2026-07-20 17:08:01 -0700

    Autosave 2026-07-21T00:08:01Z

--- engine-viewer/src/EngineViewer.tsx
+++ engine-viewer/src/EngineViewer.tsx
@@ -1398,7 +1398,7 @@ export default function EngineViewer() {
     setTurboFailure(null);
     setTurboHealthy(false);
     setFluidsChecked({ oil: false, coolant: false, washer: false, def: false, grease: false });
-    setAxleChecked({ rearAxle: false, diff: false });
+    setAxleChecked({ rearAxle: false, diff: false, fifthWheel: false, tires: false, brakes: false });
     setHoodCableStep(0);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [restoreParts, exitInspect]);
